302c68706f889b9a4bcf9885ee87f2ee06aeb7bd
Priscila <priscilasartori2@gmail.com>
2023-03-17 15:29:12 -0300

    feat: Criando a função loginUsers no controller

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from 'express';
 
-import { IUsers } from '../interfaces';
+import { ILogin, IUsers } from '../interfaces';
 import usersService from '../services/usersService';
 
 const createUsers = async (req: Request, res: Response) => {
   const users = req.body as IUsers;
-  const { status, data } = await usersService(users);
+  const { status, data } = await usersService.createUsers(users);
   return res.status(status).json(data);
 };
 
-export default { createUsers };
+const loginUsers = async (req: Request<object, object, ILogin>, res: Response) => {
+  const { body } = req;
+  const { status, data } = await usersService.loginUsers(body);
+  return res.status(status).json(data);
+};
+
+export default { createUsers, loginUsers };
