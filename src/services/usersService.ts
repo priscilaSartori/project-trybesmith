439a3fb6dc5119f3f12b12e75ba6c19f203fa7bf
Priscila <priscilasartori2@gmail.com>
2023-03-17 15:21:27 -0300

    feat: Criando a função loginUsers no service

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { IUsers } from '../interfaces';
+import { IUsers, ILogin } from '../interfaces';
 import usersModel from '../models/usersModel';
 import { secret, config } from '../middlewares/jwtConfig';
 
@@ -10,4 +10,17 @@ const createUsers = async (user: IUsers) => {
   return { status: 201, data };
 };
 
-export default createUsers;
+const loginUsers = async (login: ILogin) => {
+  const users = await usersModel.loginUsers(login);
+  const validUsername = users.username.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
+  const invalidPassword = users.password.length < 6;
+  if (!users.username) return { status: 400, message: '"username" is required' };
+  if (!users.password) return { status: 400, message: '"password" is required' };
+  if (!validUsername || invalidPassword) {
+    return { status: 401, message: 'Username or password invalid' };
+  }
+  const token = jwt.sign({ users }, secret, config);
+  const data = { token, ...users };
+  return { status: 200, data };
+};
+export default { createUsers, loginUsers };
