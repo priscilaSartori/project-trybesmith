5b1261fff10d6f8c5c2a7494da969cf19481dbc1
Priscila <priscilasartori2@gmail.com>
2023-03-18 13:58:24 -0300

    feat: Criando as validações das pessoas usuarias

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
+import validatesUsers from '../middlewares/validatesUsers';
 
 const router = Router();
-router.post('/', usersController.createUsers);
+router.post(
+  '/', 
+  validatesUsers.validatesUsername,
+  validatesUsers.validatesVocation,
+  validatesUsers.validatesLevel,
+  validatesUsers.validatesPassword,
+  usersController.createUsers,
+);
 
 export default router;
