60cc36ebdbdedecf6eae291e53b951948983b5c1
Priscila <priscilasartori2@gmail.com>
2023-03-18 15:19:35 -0300

    Criando rota para a função createOrder

--- src/routes/ordersRouter.ts
+++ src/routes/ordersRouter.ts
@@ -5,5 +5,6 @@ import ordersController from '../controllers/ordersController';
 const router = Router();
 
 router.get('/', ordersController.getAll);
+router.post('/', ordersController.createOrder);
 
 export default router;
